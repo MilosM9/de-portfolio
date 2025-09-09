d5d509329cde3580acf792bfeff41e1ea8de9dee
Milos Milicevic <milos.milicevic@aol.com>
2025-09-09 21:25:46 +0200

    Ubaceni dagovi
    	modified:   dags/spark_job.py
    	modified:   dags/spark_movies_dag.py
    	modified:   docker-compose.yaml

--- dags/spark_job.py
+++ dags/spark_job.py
@@ -1,31 +1,38 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, row_number
+from pyspark.sql.window import Window
 
-if __name__ == "__main__":
-    spark = SparkSession.builder.appName("MinIO to Postgres ETL").getOrCreate()
+# Kreiramo SparkSession. Ako Spark ima master url, koristimo njega.
+spark = (
+    SparkSession.builder.appName("MovieProcessingJob")
+    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.2")
+    .config("spark.hadoop.fs.s3a.access.key", "minio")
+    .config("spark.hadoop.fs.s3a.secret.key", "minio123")
+    .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000")
+    .config("spark.hadoop.fs.s3a.path.style.access", "true")
+    .getOrCreate()
+)
 
-    # Podešavanja za MinIO
-    spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "http://minio:9000")
-    spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", "minio")
-    spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "minio123")
-    spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
-    spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+# Osiguravamo da su podaci dostupni sa MinIO-a
+df = spark.read.csv("s3a://data-lake/IMDB Movies 2000 - 2020.csv", header=True, inferSchema=True)
 
-    # Čitanje sa MinIO
-    df = spark.read.csv("s3a://imdb/IMDB Movies 2000 - 2020.csv", header=True, inferSchema=True)
+# Čistimo podatke: Filtriramo filmove bez trajanja i zaokružujemo trajanje
+cleaned_df = df.filter(col("duration").isNotNull() & (col("duration") > 0)).withColumn(
+    "duration", col("duration").cast("integer")
+)
 
-    # Transformacija
-    filtered_df = df.filter(col("year") > 2018).select("title", "year", "avg_vote")
+# Kreiramo Window funkciju za rangiranje filmova unutar svake godine
+windowSpec = Window.partitionBy("year").orderBy(col("duration").desc())
 
-    # Upis u PostgreSQL
-    filtered_df.write \
-        .format("jdbc") \
-        .option("url", "jdbc:postgresql://postgres:5432/airflow") \
-        .option("dbtable", "top_movies") \
-        .option("user", "airflow") \
-        .option("password", "airflow") \
-        .option("driver", "org.postgresql.Driver") \
-        .mode("overwrite") \
-        .save()
+# Pronalazimo najduži film po godini
+longest_movie_per_year = cleaned_df.withColumn("row_number", row_number().over(windowSpec)).filter(
+    col("row_number") == 1
+).select("year", "title", "duration")
 
-    spark.stop()
+# Snimamo obrađene podatke u novi CSV fajl u MinIO-u
+output_path = "s3a://data-lake/longest_movie_per_year.csv"
+longest_movie_per_year.write.mode("overwrite").csv(output_path, header=True)
+
+print("Spark posao uspješno završen. Obrađeni podaci su snimljeni u MinIO.")
+
+spark.stop()
--- dags/spark_movies_dag.py
+++ dags/spark_movies_dag.py
@@ -1,7 +1,6 @@
 from airflow.decorators import dag, task
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from pendulum import datetime
-from os import environ
 
 @dag(
     dag_id="spark_movie_processing_pipeline",
@@ -9,7 +8,6 @@ from os import environ
     start_date=datetime(2023, 10, 26),
     catchup=False,
     tags=["spark", "data-processing", "movies"],
-    doc_md=__doc__,
 )
 def spark_movie_processing_pipeline():
     """
@@ -17,13 +15,12 @@ def spark_movie_processing_pipeline():
     Čita CSV datoteku s MinIO-a, pronalazi najduži film po godini,
     i sprema rezultat natrag u MinIO.
     """
-    
-    # Prvi zadatak: Pokretanje Spark joba
+
     run_spark_job = SparkSubmitOperator(
         task_id="run_spark_job",
         conn_id="spark_connection",
         application="/opt/airflow/dags/spark_job.py",  # Putanja do skripte
-        conf={"spark.master": "spark://spark-master:7077", "spark.driver.host": "airflow-worker"},
+        conf={"spark.master": "spark://spark-master:7077"},
         verbose=True,
     )
 
